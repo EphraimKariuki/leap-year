8cf80b7b2b89226e573480958664d5859f3efe21
Ephraim Kariuki <ephraimkariuki1@gmail.com>
2018-07-11 11:44:56 +0300

    if condition testing if a number is divisible by 400

--- js/styles.js
+++ js/styles.js
@@ -6,10 +6,14 @@ $(document).ready(function(){
     $("#result").text(result);
   });
   var leapYear= function(year){
-    if (year % 4===0){
+    if (year%400=== 0){
+      return true;
+    }else if (year % 100 ===0){
+      return false;
+    } else if(year % 4===0){
       return true;
     }else {
       return false;
-    }  
+    }
   };
 });
